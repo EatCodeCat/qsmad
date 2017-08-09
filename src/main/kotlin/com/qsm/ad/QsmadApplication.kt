ecd9fdeed3d648f835284a78c32221e9108df071
liangjian <1738757@qq.com>
2017-08-09 22:08:19 +0800

    first commit

---
+++ src/main/kotlin/com/qsm/ad/QsmadApplication.kt
@@ -0,0 +1,12 @@
+package com.qsm.ad
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class QsmadApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(QsmadApplication::class.java, *args)
+    
+}
---
+++ src/test/kotlin/com/qsm/ad/QsmadApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.qsm.ad
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class QsmadApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
